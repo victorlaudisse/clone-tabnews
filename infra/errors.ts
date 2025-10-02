ffa0c03259530166d46c4976ad6c9d19db5fb680
Victor Laudisse <123394866+victorlaudisse@users.noreply.github.com>
2025-10-02 05:55:27 +0000

    feat: create and use `InternalServerError` in `api/v1/status`

---
+++ infra/errors.ts
@@ -0,0 +1,22 @@
+export class InternalServerError extends Error {
+  private action: string;
+  private statusCode: number;
+
+  constructor({ cause }) {
+    super("Um erro interno não esperado aconteceu.", {
+      cause,
+    });
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte.";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
