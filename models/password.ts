3ede090a2b563caa6baf67e87601d8d459a23b45
Victor Laudisse <123394866+victorlaudisse@users.noreply.github.com>
2026-02-13 04:07:35 +0000

    feat: add `peper` to `password`

--- models/password.ts
+++ models/password.ts
@@ -2,16 +2,22 @@ import bcrypt from "bcryptjs";
 
 async function hash(password: string) {
   const rounds = getNumberOfRounds();
-  return await bcrypt.hash(password, rounds);
+  const peper = getPasswordPeper();
+  return await bcrypt.hash(password + peper, rounds);
 }
 
 async function compare(providedPassword: string, storedPassword: string) {
-  return await bcrypt.compare(providedPassword, storedPassword);
+  const peper = getPasswordPeper();
+  return await bcrypt.compare(providedPassword + peper, storedPassword);
 }
 
 function getNumberOfRounds() {
   const rounds = process.env.NODE_ENV === "production" ? 14 : 1;
   return rounds;
+}
+
+function getPasswordPeper() {
+  return process.env.PASSWORD_PEPER;
 }
 
 const password = { hash, compare };
